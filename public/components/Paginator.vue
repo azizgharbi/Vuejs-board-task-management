405577e8b91a00b27749cc62d1c4caa223fa7e77
aziz gharbi <med.aziz.gharbii@gmail.com>
2018-03-08 23:35:39 -0500

    paginator component

---
+++ public/components/Paginator.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="position" v-if="elements > perPage">
+      <nav class="pagination" role="navigation" aria-label="pagination">
+  <a class="pagination-previous" title="This is the first page" disabled>Previous</a>
+  <a class="pagination-next">Next page</a>
+  <ul class="pagination-list" v-for="(element, index) in paginationLink">
+    <li>
+      <a class="pagination-link" aria-label="Page 1" aria-current="page">{{index + 1 }}</a>
+    </li>
+  </ul>
+</nav>
+  </div>
+</template>
+<script>
+   export default {
+     name: "paginator",
+     props: ['perPage'],
+     data() {
+       return {
+         
+       }
+    },
+    computed: {
+       elements(){
+         return  Math.floor(this.$store.state.cards.length);
+       },
+        paginationLink(){
+         return  Math.floor(this.$store.state.cards.length / this.perPage);
+       }
+     },
+     methods:{
+       
+     }
+   };
+</script>
+<style>
+.position{
+    margin: 10px;
+}
+</style>
