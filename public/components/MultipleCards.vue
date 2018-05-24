18550e408d06c0c69cbf7dadbd5f4e1ef1deb4b6
aziz gharbi <agharbi@inovva.com>
2018-05-24 11:40:35 -0400

    add remove multiple form component

--- public/components/MultipleCards.vue
+++ public/components/MultipleCards.vue
@@ -2,33 +2,40 @@
    <div>
     <div class="columns is-mobile">
         <div class="column is-2 is-offset-10">
-            <a class="button is-success" @click="addForm">Add card</a>
+            <a class="button is-success" @click="addForm">Add Form</a>
         </div>
     </div>
-    <div v-for="formElment in form" :key="formElment.id">
+    <div v-for=" (formElment,index) in form" :key="formElment.id">
+        <div class="columns is-mobile">
+            <div class="column is-2 is-offset-10">
+                <span class="deleteButton"><a class="delete is-medium" @click="deleteForm(index)"></a></span>
+            </div>
+        </div>
       <div class="field">
          <label class="label">Title</label>
          <div class="control">
-            <input class="input" type="text" placeholder="Example : RSP-452">
+            <input class="input" type="text" placeholder="Example : RSP-452" v-model="formElment.title">
          </div>
       </div>
       <textarea 
+         v-model="formElment.description"
          class="textarea" 
          placeholder="Explain what do you want clearly">
    </textarea>
     <section class="radio-section">
         <div class="block">
             <b-radio 
+                v-model="formElment.status"
                 native-value="To do">
                          To do
-
             </b-radio>
             <b-radio 
+                v-model="formElment.status"
                 native-value="In progress">
                          In progress
-
             </b-radio>
             <b-radio
+                v-model="formElment.status"
                 native-value="Done">
                 Done
             </b-radio>
@@ -36,7 +43,7 @@
     </section>
     </div>
       <div class="row">
-       <a class="button is-primary" @click="AddToBoardComponent()">Create</a>
+       <a class="button is-primary" @click="createCards()">Create</a>
        <a class="button is-warning" @click="Clear()">Clear</a>
       </div>
    </div>
@@ -46,12 +53,33 @@
      name: "MultipleCreate",
      data() {
        return {
-        form: []
+        form: [{
+            title:'',
+            status: 'To do',
+            description:''
+        }]
        }
     },
      methods:{
          addForm(){
-             this.form.push({id : Math.random()})
+             this.form.push({
+                title : this.form.title,
+                status : this.form.status,
+                description : this.form.description,
+             })
+         },
+         createCards(){
+             this.form.forEach(element => {
+                 let card = {
+                     title : element.title,
+                     status : element.status,
+                     description : element.description
+                 }
+                 this.$store.commit('AddCardTodatabase',card);
+             });
+         },
+         deleteForm(index){
+             this.form.splice(index,1);
          }
      }
    };
